510e440cf9407a891cc3f51b129d942497bef575
jreed <jjreed@ucsd.edu>
2016-04-11 13:10:54 -0700

    my commit message

---
+++ LinkAssignment/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  LinkAssignment
+//
+//  Created by J REED on 4/11/16.
+//  Copyright (c) 2016 J REED. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        println("I cant wait to put this on github");
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
